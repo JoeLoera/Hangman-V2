febafe3dd4fbf23602807278c649fe90d7519267
Nova Loera <60495917+JoeLoera@users.noreply.github.com>
2022-07-04 00:03:13 +0000

    Game is currently functioning with unlimited guesses.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def hangman():
   used_letters = set() #what the user has guessed
 
   #getting user input
+  while_
   user_letter = input('Guess a letter: ').upper()
   if user_letter in alphabet - used_letters:
     used_letters.add(user_letter)
